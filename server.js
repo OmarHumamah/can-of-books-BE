824cd0b2e31ebbdbd319dee2b87548db2f704e75
OmarHumamah <omar.nabeel.h@gmail.com>
2021-09-13 16:15:17 +0300

    lab11

--- server.js
+++ server.js
@@ -1,18 +1,85 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
-app.get('/test', (request, response) => {
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
-  response.send('test request received')
+//..................MONGOOOS...................
+const mongoose = require("mongoose");
+let booksModal;
 
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/books-list");
+
+  const bookSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    status: String,
+    email: String,
+  });
+
+  booksModal = mongoose.model("books", bookSchema);
+
+  // will uncomment this \/ function whin we need to save
+  //seedData();
+}
+
+//.....\\.. to seed the modal..//......
+async function seedData() {
+  const cleanCode = new booksModal({
+    title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    description: 'Programming is about polishing the craft with years of trial and error. I wish there was a way to save yourself from all the hard work by learning from the mistakes of other programmers? Fortunately, there is, and it is known to the world as the Clean Code: A Handbook of Agile Software Craftsmanship book from the legendary Uncle Bob.',
+    status: true,
+    email: 'omar.nabeel.h@gmail.com',
+  });
+  const introductionToAlgorithms = new booksModal({
+    title: 'Introduction to Algorithms',
+    description: 'The name of the book is self-explanatory. It is what the title suggests, i.e., Introduction to Algorithms. Also known as CLRS, a reference to the last name of the authors of the book, it goes in-depth into a range of algorithms divided across several self-contained chapters.',
+    status: false,
+    email: 'omar.nabeel.h@gmail.com',
+  });
+  const codeComplete = new booksModal({
+    title: 'Code Complete: A Practical Handbook of Software Construction',
+    description: 'Want to know how to write robust code irrespective of the architecture of a programming language? Then consider reading the Code Complete: A Practical Handbook of Software Construction. It comprehensively covers all the aspects of the structure of good code.',
+    status: true,
+    email: 'omar.nabeel.h@gmail.com',
+  });
+
+  await cleanCode.save();
+  await introductionToAlgorithms.save();
+  await codeComplete.save();
+}
+//..............FINNISH WITH DB...............................
+
+//..............ROUTS.........................................
+app.get("/books", getBookHandler );
+
+function getBookHandler(request, response){
+  let email = request.query.email;
+  booksModal.find({email : email},(err,result)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+      response.send(result);
+      console.log(result);
+    }
 })
+}
+
+//..................and that's it...............................
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
