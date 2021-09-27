cefef07e16bffe6bc21e73a98c81d71440fca7cd
OmarHumamah <omar.nabeel.h@gmail.com>
2021-09-27 12:27:15 +0300

    test02

--- server.js
+++ server.js
@@ -6,108 +6,103 @@ const cors = require("cors");
 const server = express();
 server.use(cors());
 server.use(express.json());
-const { default: axios } = require("axios");
-let PORT = 3001;
-const mongoose = require("mongoose");
-let movieModal;
-
-main().catch((err) => console.log(err));
+let PORT = process.env.PORT;
+let {default: axios}= require('axios');
+const mongoose = require('mongoose');
+let flowersModal;
+main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(
-    `mongodb://Omar_Humamah:123Omar321@omarcluster-shard-00-00.reqod.mongodb.net:27017,omarcluster-shard-00-01.reqod.mongodb.net:27017,omarcluster-shard-00-02.reqod.mongodb.net:27017/favMove?ssl=true&replicaSet=atlas-104q19-shard-0&authSource=admin&retryWrites=true&w=majority`
-  );
+  await mongoose.connect(process.env.MONGO_DB);
 
-  const movieSchema = new mongoose.Schema({
-    title: String,
-    img: String,
-    description: String,
-    email: String,
+  const flowersSchema = new mongoose.Schema({
+    name: String,
+    instructions: String,
+    photo: String,
+    email: String
   });
 
-  movieModal = mongoose.model("favMovie", movieSchema);
+  flowersModal = mongoose.model('flower', flowersSchema);
 }
 
-server.get("/movies", gitMovies);
-server.post("/addFav", addToFav);
-server.get("/getFav", gitFavMovies);
-server.delete("/delete/:id", deleteMove);
-server.put("/update/:id", updateMovie);
 
-function gitMovies(req, res) {
+
+server.get('/getflowers', getFlowers);
+server.post('/addtofav', addToFav);
+server.get('/getfavflowers', getFavFlowers);
+server.delete('/delete/:id', deleteFlower)
+server.put('/update/:id', updateFlower )
+
+function getFlowers(req, res){
   axios
-    .get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=d392d3ce0f9ceefcfd09b99d97173b24&language=en-US&page=1`
-    )
-    .then((result) => {
-      res.send(result.data.results);
-    })
-    .catch((err) => console.log(err));
+  .get('https://flowers-api-13.herokuapp.com/getFlowers')
+  .then(result=>{
+    res.send(result.data.flowerslist)
+  }
+  )
 }
 
-async function addToFav(req, res) {
-  let { title, img, description, email } = req.body;
-
-  await movieModal.create({
-    title: title,
-    img: img,
-    description: description,
-    email: email,
-  });
-
-  movieModal.find({ email }, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
+async function addToFav (req, res){
+  let email = req.query.email
+  let {instructions, photo, name  } = req.body;
+  
+ await flowersModal.create({ 
+  name: name,
+  instructions: instructions,
+  photo: photo,
+  email: email
+ })
+ 
+ flowersModal.find({email}, (err, result)=>{
+   if(err){
+     console.log(err);
+   }else{
+     res.send(result)
+   }
+ })
 }
 
-function gitFavMovies(req, res) {
+function getFavFlowers (req, res){
   let email = req.query.email;
-
-  movieModal.find({ email }, (err, result) => {
-    if (err) {
+  flowersModal.find({email}, (err, result)=>{
+    if(err){
       console.log(err);
-    } else {
-      res.send(result);
+    }else{
+      res.send(result)
     }
-  });
+  })
 }
 
-function deleteMove(req, res) {
+function deleteFlower(req, res){
+let id = req.params.id;
+let email = req.query.email;
+
+flowersModal.deleteOne({_id:id}, (err, result)=>{
+  flowersModal.find({email}, (err, result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.send(result)
+    }
+  })
+})
+
+}
+
+function updateFlower (req,res){
   let id = req.params.id;
   let email = req.query.email;
+  let {name, instructions} = req.body;
 
-  movieModal.deleteOne({ _id: id }, (err, result) => {
-    movieModal.find({ email }, (err, result) => {
-      if (err) {
+  flowersModal.findByIdAndUpdate({_id:id}, {name, instructions}, (err, result)=>{
+    flowersModal.find({email}, (err, result)=>{
+      if(err){
         console.log(err);
-      } else {
-        res.send(result);
+      }else{
+        res.send(result)
       }
-    });
-  });
+    })
+  })
 }
 
-function updateMovie(req, res) {
-  let id = req.params.id;
-  let { title, description, email } = req.body;
-
-  movieModal.findByIdAndUpdate(
-    { _id: id },
-    { title, description, email },
-    (err, result) => {
-      movieModal.find({ email }, (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      });
-    }
-  );
-}
-
-server.listen(PORT, () => console.log(`listening on 3001`));
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
